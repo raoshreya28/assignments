ac20ef2286aeaa8e987ee6172531a6da083e64f0
raoshreya2804 <141177813+raoshreya28@users.noreply.github.com>
2025-06-16 22:56:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp_factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number:");
+            int num=Convert.ToInt32(Console.ReadLine());
+            int fact = 1;
+            for (int i = 1; i <=num; i++) {
+                fact = fact * i;
+            }
+            Console.WriteLine("The factorial of " + num + " is " + fact);
+
+        }
+    }
+}
